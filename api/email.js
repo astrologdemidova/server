44ea55598d5865640e08a04f64eee7c015bc6222
astrologdemidova <80847686+astrologdemidova@users.noreply.github.com>
2021-06-20 19:26:40 +0300

    add email templates

--- api/email.js
+++ api/email.js
@@ -2,6 +2,7 @@ const PORT = process.env.PORT || 8080;
 const fs = require('fs');
 const { nanoid } = require('nanoid');
 const nodemailer = require('nodemailer');
+const { textTemplates } = require('./helpers/textTemplates');
 
 /*
 
@@ -43,11 +44,14 @@ const getInfoData = () => {
 /** POST /api/email/add-user-contact */
 console.log(`##email http://localhost:${PORT}/api/email/add-user-contact`);
 exports.toAddUserContact = (req, res) => {
+    const notificationIdPrize = ['001', '002', '003'];
+
     const mapUserData = {
         name: req.query.name,
         email: req.query.email,
         phone: req.query.phone,
         inst: req.query.inst,
+        idPrize: req.query.id,
     };
     userContact.push({ ...getInfoData(), ...mapUserData });
     var json = JSON.stringify(userContact);
@@ -57,12 +61,22 @@ exports.toAddUserContact = (req, res) => {
         res.status(200).send(`success`);
     });
 
-    // send message to user
-    const message = {
+    // send message TO USER
+    const messageUser = {
         from: process.env.EMAIL_LOGIN, // Sender address
-        to: `${req.query.email}, ${process.env.EMAIL_LOGIN}`, // List of recipients
+        to: `${req.query.email}`, // List of recipients
         subject: 'shop.astrologdemidova.ru | Колесо фортуны',
-        html: 'This <i>message</i> with <strong>attachments</strong>.',
+        html: `
+        <body>
+            <table>
+                ${
+                    textTemplates[req.query.id].map((row) => {
+                        return `<tr><td>${row}</td></tr>`
+                    })
+                }
+            </table>
+        </body>
+        `,
         // attachments: [
         //     {
         //         filename: 'greetings.txt',
@@ -76,13 +90,69 @@ exports.toAddUserContact = (req, res) => {
     };
 
 
-    transport.sendMail(message, function (err, info) {
+    transport.sendMail(messageUser, function (err, info) {
         if (err) {
             console.log(err)
         } else {
             console.log(info);
         }
     });
+
+    // send message TO CUSTOMER
+    const messageCustomer = {
+        from: process.env.EMAIL_LOGIN, // Sender address
+        to: `${process.env.EMAIL_LOGIN}`, // List of recipients
+        subject: 'shop.astrologdemidova.ru | Колесо фортуны',
+        html: `
+        <body>
+            <table>
+                <tr>
+                    <td>Новый выигрыш от "Колесо фортуны"</td>
+                </tr>
+            </table>
+            <table>
+                <tr>
+                    <th>Приз</th>
+                    <th>Имя</th>
+                    <th>Номер</th>
+                    <th>Почта</th>
+                    <th>Инстаграм</th>
+                    <th>Адресс</th>
+                </tr>
+                <tr>
+                    <td>${req.query.id}</td>
+                    <td>${req.query.name}</td>
+                    <td>${req.query.phone}</td>
+                    <td>${req.query.email}</td>
+                    <td>${req.query.inst}</td>
+                    <td>${req.query.adress}</td>
+                </tr>
+            </table>
+            <table>
+                <tr>
+                    <td>А ниже копия письма для юзера:</td>
+                </tr>
+            </table>
+            <table>
+                ${
+                    textTemplates[req.query.id].map((row) => {
+                        return `<tr><td>${row}</td></tr>`
+                    })
+                }
+            </table>
+        </body>
+        `,
+    };
+
+    if (notificationIdPrize.includes(req.query.id)) { // send only for notificationIdPrize
+        transport.sendMail(messageCustomer, function (err, info) {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log(info);
+            }
+        });
+    }
 };
 
 
