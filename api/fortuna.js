c34a361a669d7dd2da3f2339882c2f609ae122ee
astrologdemidova <80847686+astrologdemidova@users.noreply.github.com>
2021-06-21 11:16:48 +0300

    test

--- api/fortuna.js
+++ api/fortuna.js
@@ -26,6 +26,12 @@ exports.toGetWheelItem = (req, res) => {
         '004',
         '006',
         '008',
+        '001',
+        '003',
+        '005',
+        '007',
+        '009',
+        '010',
     ];
 
     const getRandomInt = (max) => {
